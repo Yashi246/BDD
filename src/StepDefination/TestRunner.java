110a0ccd812d7e7b75b0aa313b51e4de218faf74
Yashi246 <51956340+Yashi246@users.noreply.github.com>
2019-07-31 17:10:15 +0530

    Add files via upload

---
+++ src/StepDefination/TestRunner.java
@@ -0,0 +1,24 @@
+package StepDefination;
+
+import org.junit.runner.RunWith;
+
+
+
+import cucumber.api.CucumberOptions;
+
+import cucumber.api.junit.Cucumber;
+
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features= {"src/login/features"},glue= {"stepDefinition"})
+
+public class TestRunner {
+
+
+
+
+
+}
+
---
+++ src/login/features/login.feature
@@ -0,0 +1,12 @@
+Feature: Login
+
+User has a link www.gmail.com
+
+Scenario: User will login on www.gmail.com
+
+Given User will enter valid username and password
+
+When User will click login button
+
+Then User should see his/her account home page
+
